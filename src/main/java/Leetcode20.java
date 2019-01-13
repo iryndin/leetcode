f01ebd9cec8fb72012585503287f276bac1d5ed9
iryndin <iryndin@gmail.com>
2019-01-13 17:16:39 -0500

    Add Leetcode20

---
+++ src/main/java/Leetcode20.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Leetcode20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (isOpening(c)) {
+                stack.push(c);
+            } else if (isClosing(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char d = stack.pop();
+                if (!isComplement(d,c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    boolean isClosing(char c) {
+        return c == ')' ||
+                c == '}' ||
+                c == ']';
+    }
+
+    boolean isOpening(char c) {
+        return c == '(' ||
+                c == '{' ||
+                c == '[';
+    }
+
+    boolean isComplement(char a, char b) {
+        switch (a) {
+            case '(': return b == ')';
+            case '[': return b == ']';
+            case '{': return b == '}';
+        }
+        return false;
+    }
+}
