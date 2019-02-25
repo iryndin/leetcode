d9299bf8364c012fbc586724aa3c8db0e2cdce36
iryndin <iryndin@gmail.com>
2019-02-24 22:01:00 -0500

    Added: 654

---
+++ src/main/java/Leetcode654.java
@@ -0,0 +1,36 @@
+public class Leetcode654 {
+
+    static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return maxtree(nums, 0, nums.length);
+    }
+
+    static TreeNode maxtree(int[] a, int start, int end) {
+        int max = Integer.MIN_VALUE;
+        int maxIdx = -1;
+
+        // 1. find max element
+        for (int i=start; i<end; i++) {
+            if (a[i] > max) {
+                max = a[i];
+                maxIdx = i;
+            }
+        }
+        if (maxIdx == -1) {
+            return null;
+        }
+        TreeNode n = new TreeNode(max);
+        n.left = maxtree(a, start, maxIdx);
+        n.right = maxtree(a, maxIdx+1, end);
+        return n;
+    }
+
+
+
+}
